054c9c8f45942628f23a3e2ac8ea77e56b70cfb7
Paulo Koch <pauloricardokoch@gmail.com>
2026-09-15 22:02:01 -0300

    update

--- lua/custom/plugins/nvimtree.lua
+++ lua/custom/plugins/nvimtree.lua
@@ -4,7 +4,7 @@
 -- See the kickstart.nvim README for more information
 return {
   'nvim-tree/nvim-tree.lua',
-  enabled = true,
+  enabled = false,
   version = '*',
   lazy = false,
   dependencies = {
